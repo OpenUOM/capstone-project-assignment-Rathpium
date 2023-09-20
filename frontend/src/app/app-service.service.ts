4aaa2df0aa54512c76a6763341b237e7c22629e5
Eranda Rathpium <rathpium@gmail.com>
2023-09-20 15:59:19 +0530

    test /

--- frontend/src/app/app-service.service.ts
+++ frontend/src/app/app-service.service.ts
@@ -7,9 +7,9 @@ import { environment } from './../environments/environment';
 })
 export class AppServiceService {
 
-  readonly ROOT_URL;
+  readonly ROOT_URL: string;
 
-  constructor(private http: HttpClient) {
+  /*constructor(private http: HttpClient) {
     if(environment.production == false){
       this.ROOT_URL = 'test'
     }else{
@@ -60,4 +60,52 @@ export class AppServiceService {
   deleteStudent(payload: Object){
     return this.http.post(`/${this.ROOT_URL}/deleteStudent`, payload)
   }
+}*/
+constructor(private http: HttpClient) {
+  // Set the base URL based on the environment
+  this.ROOT_URL = environment.production ? 'http://localhost:4200/' : 'http://localhost:4200/test/';
+}
+
+initializeDB(){
+  return this.http.get(`/${this.ROOT_URL}dbinitialize`)
+}
+
+getTeacherData(){
+  return this.http.get(`/${this.ROOT_URL}listTeachers`)
+}
+
+getStudentData(){
+  return this.http.get(`/${this.ROOT_URL}listStudents`)
+}
+
+getOneStudentData(payload: Object){
+  return this.http.post(`/${this.ROOT_URL}getStudentInfo`, payload)
+}
+
+getOneTeacherData(payload: Object){
+  return this.http.post(`/${this.ROOT_URL}getTeacherInfo`, payload)
+}
+
+addTeacher(payload: Object){
+  return this.http.post(`/${this.ROOT_URL}addTeacher`, payload)
+}
+
+deleteTeacher(payload: Object){
+  return this.http.post(`/${this.ROOT_URL}deleteTeacher`, payload)
+}
+
+editTeacher(payload: Object){
+  return this.http.post(`/${this.ROOT_URL}editTeacher`, payload)
+}
+
+editStudent(payload: Object){
+  return this.http.post(`/${this.ROOT_URL}editStudent`, payload)
+}
+
+addStudent(payload: Object){
+  return this.http.post(`/${this.ROOT_URL}addStudent`, payload)
+}
+
+deleteStudent(payload: Object){
+  return this.http.post(`/${this.ROOT_URL}deleteStudent`, payload)
 }
